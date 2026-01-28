8bb7daf9585841d21b60ab9cf9c187390f80e664
Jwiroo <qorudgus77@gmail.com>
2026-01-28 15:45:07 +0900

    feat(header): add logo

--- components/layout/header/Header.tsx
+++ components/layout/header/Header.tsx
@@ -1,10 +1,11 @@
+import Logo from '@/components/common/logo/Logo';
 import styles from './header.module.scss';
 
 export default function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.layout}>
-        
+        <Logo width={70} height={32} />
       </div>
     </header>
   );
