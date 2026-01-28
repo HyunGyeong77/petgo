1b48a61370644e668064ef67fe402928a44a1e32
Jwiroo <qorudgus77@gmail.com>
2026-01-28 15:14:53 +0900

    refactor(common): move SafeLink into safe-link folder and add styles

--- components/common/safe-link/SafeLink.tsx
+++ components/common/safe-link/SafeLink.tsx
@@ -1,10 +1,11 @@
 "use client";
 
+import styles from './safe-link.module.scss';
 import Link from 'next/link';
 
 export default function SafeLink({children}: Readonly<{children: React.ReactNode}>) {
   return (
-    <Link href="#" aria-label="임시 링크" onClick={(e) => {e.preventDefault();}}>
+    <Link className={styles.link} href="#" aria-label="임시 링크" onClick={(e) => {e.preventDefault();}}>
       {children}
     </Link>
   );
