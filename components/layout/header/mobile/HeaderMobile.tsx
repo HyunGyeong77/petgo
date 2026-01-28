0f65681937c2d58b699b8cf0c2f9b7d7464af4cc
Jwiroo <qorudgus77@gmail.com>
2026-01-28 16:25:59 +0900

    refactor(header): move the contents and styles of the header folder to the mobile folder

---
+++ components/layout/header/mobile/HeaderMobile.tsx
@@ -0,0 +1,12 @@
+import Logo from '@/components/common/logo/Logo';
+import styles from './header.module.scss';
+
+export default function HeaderMobile() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.layout}>
+        <Logo width={70} height={32} />
+      </div>
+    </header>
+  );
+}
