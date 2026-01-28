aaf374319ecb55310a1b40ec0dc6e57b4e502ffd
Jwiroo <qorudgus77@gmail.com>
2026-01-28 21:43:54 +0900

    style(header): add styles for login and join buttons

--- components/layout/header/mobile/components/hamburger/components/menu-panel/MenuPanel.tsx
+++ components/layout/header/mobile/components/hamburger/components/menu-panel/MenuPanel.tsx
@@ -1,6 +1,7 @@
 import styles from './menu-panel.module.scss';
 import Close from '../../assets/close.svg';
 import Image from 'next/image';
+import SafeLink from '@/components/common/safe-link/SafeLink';
 
 export default function MenuPanel({onClick}: {onClick: (bool: boolean) => void}) {
   return (
@@ -11,6 +12,10 @@ export default function MenuPanel({onClick}: {onClick: (bool: boolean) => void})
             <button className={styles["close-btn"]} onClick={() => onClick(false)} aria-label="close button">
               <Image src={Close} alt="close image" width={25} height={25} />
             </button>
+            <div className={styles["join-layout"]}>
+              <SafeLink>로그인</SafeLink>
+              <SafeLink>회원가입</SafeLink>
+            </div>
           </div>
         </div>
       </div>
