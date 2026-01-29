d3909086d15bcd2900e9e3dfc906a9e4aa4ace5c
Jwiroo <qorudgus77@gmail.com>
2026-01-29 15:38:38 +0900

    style(hero): abjust background image

--- app/components/home/hero/Hero.tsx
+++ app/components/home/hero/Hero.tsx
@@ -1,9 +1,13 @@
 import styles from './hero.module.scss';
+import Background from './assets/images/bg.jpg';
+import Image from 'next/image';
 
 export default function Hero() {
   return (
     <div className={styles.wrap}>
-      
+      <div className={styles["bg-box"]}>
+        <Image className={styles.bg} src={Background} alt="background image" width={1920} height={1275} />
+      </div>
     </div>
   );
 }
