5e07c34460a33ffa2c5a741b72d059980785119b
Jwiroo <qorudgus77@gmail.com>
2026-01-29 20:24:17 +0900

    refactor(hero): make layout and fonts responsive to desktop view

--- app/components/home/hero/Hero.tsx
+++ app/components/home/hero/Hero.tsx
@@ -2,12 +2,15 @@
 
 import styles from './hero.module.scss';
 import Background from './assets/images/bg.jpg';
+import Dog from './assets/images/dog.png';
 import DownIcon from './assets/gifs/down.gif';
 import Image from 'next/image';
 import {useRef, useEffect} from 'react';
+import {useMediaQuery} from '@/lib/hooks/useMediaQuery';
 import gsap from 'gsap';
 
 export default function Hero() {
+  const isDesktop = useMediaQuery("(min-width:1025px)");
   const titleRef = useRef<HTMLHeadingElement | null>(null);
   const contentRef = useRef<HTMLParagraphElement | null>(null);
   const notifyRef = useRef<HTMLDivElement | null>(null);
@@ -32,8 +35,13 @@ export default function Hero() {
       </div>
       <div className={styles["content-layout"]}>
         <div className={styles.content}>
-          <h2 ref={titleRef}>반려견을 더 잘 이해하는 방법</h2>
-          <p ref={contentRef} className={styles.word}>강아지가 좋아하는 것부터 산책, 용품, 병원 정보까지 한 곳에서 확인하세요.</p>
+          {isDesktop &&
+            <Image className={styles["dog-img"]} src={Dog} alt="dog image" width={450} height={354} />
+          }
+          <div className={styles.group}>
+            <h2 className={styles.heading} ref={titleRef}>반려견을 더 잘 이해하는 방법</h2>
+            <p ref={contentRef} className={styles.word}>강아지가 좋아하는 것부터 산책, 용품, 병원 정보까지 한 곳에서 확인하세요.</p>
+          </div>
         </div>
         <div ref={notifyRef} className={styles.notify}>
           <Image className={styles["down-img"]} src={DownIcon} alt="down icon" width={17} height={13} />
