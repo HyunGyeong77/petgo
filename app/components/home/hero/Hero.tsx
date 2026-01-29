b34de7b66a772a3cd3f0027d3340dc12de5fec88
Jwiroo <qorudgus77@gmail.com>
2026-01-29 18:06:23 +0900

    feat(hero): add GSAP animation

--- app/components/home/hero/Hero.tsx
+++ app/components/home/hero/Hero.tsx
@@ -1,9 +1,30 @@
+"use client";
+
 import styles from './hero.module.scss';
 import Background from './assets/images/bg.jpg';
 import DownIcon from './assets/gifs/down.gif';
 import Image from 'next/image';
+import {useRef, useEffect} from 'react';
+import gsap from 'gsap';
 
 export default function Hero() {
+  const titleRef = useRef<HTMLHeadingElement | null>(null);
+  const contentRef = useRef<HTMLParagraphElement | null>(null);
+  const notifyRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const titleCur = titleRef.current;
+    const contentCur = contentRef.current;
+    const notifyCur = notifyRef.current;
+
+    if(!titleCur || !contentCur || !notifyCur) return;
+
+    const timeline = gsap.timeline();
+    timeline.fromTo(titleCur, {y:50, opacity:0}, {y: 0, opacity:1, duration:0.7})
+      .fromTo(contentCur, {y:30, opacity:0}, {y:0, opacity:1, duration: 0.7}, "-=0.3")
+      .fromTo(notifyCur, {y:30, opacity:0}, {y:0, opacity:1, duration: 0.7}, "-=0.3")
+  }, []);
+
   return (
     <div className={styles.wrap}>
       <div className={styles["bg-box"]}>
@@ -11,10 +32,10 @@ export default function Hero() {
       </div>
       <div className={styles["content-layout"]}>
         <div className={styles.content}>
-          <h2>반려견을 더 잘 이해하는 방법</h2>
-          <p className={styles.word}>강아지가 좋아하는 것부터 산책, 용품, 병원 정보까지 한 곳에서 확인하세요.</p>
+          <h2 ref={titleRef}>반려견을 더 잘 이해하는 방법</h2>
+          <p ref={contentRef} className={styles.word}>강아지가 좋아하는 것부터 산책, 용품, 병원 정보까지 한 곳에서 확인하세요.</p>
         </div>
-        <div className={styles.notify}>
+        <div ref={notifyRef} className={styles.notify}>
           <Image className={styles["down-img"]} src={DownIcon} alt="down icon" width={17} height={13} />
           <span>아래로 스크롤</span>
         </div>
