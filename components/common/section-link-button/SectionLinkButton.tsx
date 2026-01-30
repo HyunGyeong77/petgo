0af144ee409081a4af68d91ea5efd5f9252b413d
Jwiroo <qorudgus77@gmail.com>
2026-01-30 19:51:35 +0900

    feat(section-link-button): add mouse event handler

--- components/common/section-link-button/SectionLinkButton.tsx
+++ components/common/section-link-button/SectionLinkButton.tsx
@@ -1,10 +1,20 @@
 import styles from './section-link-button.module.scss';
 import SafeLink from "../safe-link/SafeLink";
+import {useState} from 'react';
 
 export default function SectionLinkButton({content}: {content: React.ReactNode}) {
+  const [isEnter, setIsEnter] = useState(false);
+
+  const mouseEnter = (bool: boolean) => {
+    setIsEnter(bool);
+  }
+
   return (
     <SafeLink>
-      <p className={styles.wrap}>{content}</p>
+      <p
+        className={styles.wrap} 
+        onMouseEnter={() => mouseEnter(true)}
+        onMouseLeave={() => mouseEnter(false)}>{!isEnter ? content : "가볼까요?"}</p>
     </SafeLink>
   );
 }
