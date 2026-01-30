3829fa274a67119c3025b7636634d75b93a6b7c8
Jwiroo <qorudgus77@gmail.com>
2026-01-30 15:45:26 +0900

    feat(preferences): add layout section

---
+++ app/components/home/preferences/Preferences.tsx
@@ -0,0 +1,12 @@
+import styles from './preferences.module.scss';
+import SectionContent from '@/components/layout/section/content/SectionContent';
+
+export default function Preferences() {
+  return (
+    <SectionContent>
+      <article className={styles.wrap}>
+
+      </article>
+    </SectionContent>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Header from '@/components/layout/header/Header';
 import SectionLayout from '@/components/layout/section/SectionLayout';
 import Hero from './components/home/hero/Hero';
+import Preferences from './components/home/preferences/Preferences';
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Header />
       <SectionLayout>
         <Hero />
+        <Preferences />
       </SectionLayout>
     </>
   );
