b616fbe093d9018aa7e4b002d1576fe19eea33b0
Jwiroo <qorudgus77@gmail.com>
2026-01-30 13:46:47 +0900

    refactor(header): unify mobile and desktop layouts

--- components/layout/header/desktop/HeaderDesktop.tsx
+++ components/layout/header/desktop/HeaderDesktop.tsx
@@ -1,7 +1,9 @@
-
+import Logo from '@/components/common/logo/Logo';
 
 export default function HeaderDesktop() {
   return (
-    <></>
+    <>
+      <Logo width={70} height={32} />
+    </>
   );
 }
--- components/layout/header/mobile/HeaderMobile.tsx
+++ components/layout/header/mobile/HeaderMobile.tsx
@@ -1,14 +1,11 @@
 import Logo from '@/components/common/logo/Logo';
 import Hamburger from './components/hamburger/Hamburger';
-import styles from './header-mobile.module.scss';
 
 export default function HeaderMobile() {
   return (
-    <header className={styles.header}>
-      <div className={styles.layout}>
-        <Logo width={70} height={32} />
-        <Hamburger />
-      </div>
-    </header>
+    <>
+      <Logo width={70} height={32} />
+      <Hamburger />
+    </>
   );
 }
