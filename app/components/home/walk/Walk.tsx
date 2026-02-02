4fe366fed49af5946417f2fde31c8422c8235b52
Jwiroo <qorudgus77@gmail.com>
2026-02-02 19:17:57 +0900

    style(walk): add style to Walk.tsx

--- app/components/home/walk/Walk.tsx
+++ app/components/home/walk/Walk.tsx
@@ -1,16 +1,17 @@
 "use client";
 
+import styles from './walk.module.scss';
 import {useMediaQuery} from "@/lib/hooks/useMediaQuery";
 import WalkMobile from "./mobile/WalkMobile";
 import WalkDesktop from "./desktop/WalkDesktop";
 
 export default function Walk() {
-  const isMobile = useMediaQuery("(max-width:600px)");
+  const isMobile = useMediaQuery("(max-width:768px)");
 
   return (
-    <article>
-      <h2>산책은 어떻게 하면 좋을까요??</h2>
-      <section>
+    <article className={styles.wrap}>
+      <h2 className={styles.title}>산책은 어떻게 하면 좋을까요??</h2>
+      <section className={styles.content}>
         {isMobile ? <WalkMobile /> : <WalkDesktop />} 
       </section>
     </article>
