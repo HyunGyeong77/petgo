1536bafb469f31262701565d9672e2553569d4a0
Jwiroo <qorudgus77@gmail.com>
2026-02-02 16:25:26 +0900

    fix(header): add parent div element about Logo component

--- components/layout/header/mobile/HeaderMobile.tsx
+++ components/layout/header/mobile/HeaderMobile.tsx
@@ -4,7 +4,7 @@ import Hamburger from './components/hamburger/Hamburger';
 export default function HeaderMobile() {
   return (
     <>
-      <Logo width={70} height={32} />
+      <div><Logo width={70} height={32} /></div>
       <Hamburger />
     </>
   );
