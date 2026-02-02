f567fcc191525510d96150b020624893aa69463b
Jwiroo <qorudgus77@gmail.com>
2026-02-02 15:26:12 +0900

    feat(preferences): replace SafeLink to SectionLinkButton

--- app/components/home/preferences/components/link/Link.tsx
+++ app/components/home/preferences/components/link/Link.tsx
@@ -1,11 +1,13 @@
+"use client";
+
 import styles from './link.module.scss';
-import SafeLink from '@/components/common/safe-link/SafeLink';
+import SectionLinkButton from '@/components/common/section-link-button/SectionLinkButton';
 
 export default function Link() {
   return (
     <section className={styles.wrap}>
       <h2>알면 알수록 반려견과의 관계는 <span>더욱 깊어집니다 😊</span></h2>
-      <SafeLink>반려견과 친해지러 가기</SafeLink>
+      <SectionLinkButton content="반려견과 친해지러 가기" />
     </section>
   );
 }
