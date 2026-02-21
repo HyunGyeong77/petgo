07887f118bbc8d0075ff982669b4ffbc0f810830
Jwiroo <qorudgus77@gmail.com>
2026-02-21 17:11:00 +0900

    feat(hospital): add react-query and toast library

---
+++ app/providers/ReactQueryProvider.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {useState} from 'react';
+import {ToastContainer} from 'react-toastify';
+
+export default function ReactQueryProvider({children}: {children: React.ReactNode}) {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ToastContainer 
+        position="top-right"
+        autoClose={2000}
+      />
+    </QueryClientProvider>
+  );
+}
