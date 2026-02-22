94ce865d68ec492e360e5755f4e890890185e70b
Jwiroo <qorudgus77@gmail.com>
2026-02-22 15:53:25 +0900

    refactor(footer): replace FooterNav style to Footer common style

--- components/layout/components/nav/FooterNav.tsx
+++ components/layout/components/nav/FooterNav.tsx
@@ -1,8 +1,6 @@
-import styles from './footer-nav.module.scss';
-
 export default function FooterNav() {
   return (
-    <ul className={styles["footer__nav"]}>
+    <ul>
       <li><a href="#top">홈</a></li>
       <li><a href="#walk">산책 가이드</a></li>
       <li><a href="#recommend">추천 용품</a></li>
