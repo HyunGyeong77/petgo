3d9a042401308c82c9b61c73f9fc43c972a0c4a5
Jwiroo <qorudgus77@gmail.com>
2026-04-03 17:29:40 +0900

    feat(hero): Modified to run animation after image loading is complete.

--- app/components/home/hero/Hero.tsx
+++ app/components/home/hero/Hero.tsx
@@ -5,7 +5,7 @@ import Background from './assets/images/bg.jpg';
 import Dog from './assets/images/dog.png';
 import DownIcon from './assets/gifs/down.gif';
 import Image from 'next/image';
-import {useRef, useEffect} from 'react';
+import {useRef, useEffect, useState} from 'react';
 import gsap from 'gsap';
 
 export default function Hero() {
@@ -13,37 +13,48 @@ export default function Hero() {
   const contentRef = useRef<HTMLParagraphElement | null>(null);
   const notifyRef = useRef<HTMLDivElement | null>(null);
   const dogRef = useRef<HTMLImageElement | null>(null);
+  const [isBgLoaded, setIsBgLoaded] = useState<boolean>(false);
 
   useEffect(() => {
-    const titleCur = titleRef.current;
-    const contentCur = contentRef.current;
-    const notifyCur = notifyRef.current;
-
-    if(!titleCur || !contentCur || !notifyCur) return;
-    
-    const before = (y: number) => {return {y: y, opacity: 0};}
-    const after = () => {return {y: 0, opacity: 1, duration: 0.7};}
-
-    const timeline = gsap.timeline();
-
-    // 반응형에 따라 시작 딜레이 값 변경
-    let delay = "0";
-    
-    if(window.innerWidth >= 1024) {
-      const dogCur = dogRef.current;
-      timeline.fromTo(dogCur, before(50), after());
-      delay = "-=0.3";
+    if(isBgLoaded) {
+      const titleCur = titleRef.current;
+      const contentCur = contentRef.current;
+      const notifyCur = notifyRef.current;
+  
+      if(!titleCur || !contentCur || !notifyCur) return;
+      
+      const before = (y: number) => {return {y: y, opacity: 0};}
+      const after = () => {return {y: 0, opacity: 1, duration: 0.7};}
+  
+      const timeline = gsap.timeline();
+  
+      // 반응형에 따라 시작 딜레이 값 변경
+      let delay = "0";
+      
+      if(window.innerWidth >= 1024) {
+        const dogCur = dogRef.current;
+        timeline.fromTo(dogCur, before(50), after());
+        delay = "-=0.3";
+      }
+  
+      timeline.fromTo(titleCur, before(50), after(), delay)
+        .fromTo(contentCur, before(30), after(), "-=0.3")
+        .fromTo(notifyCur, before(30), after(), "-=0.3");
     }
-
-    timeline.fromTo(titleCur, before(50), after(), delay)
-      .fromTo(contentCur, before(30), after(), "-=0.3")
-      .fromTo(notifyCur, before(30), after(), "-=0.3")
-  }, []);
+  }, [isBgLoaded]);
 
   return (
     <div className={styles.wrap}>
       <div className={styles["bg-box"]}>
-        <Image className={styles.bg} src={Background} alt="background image" width={1920} height={1275} loading="eager" />
+        <Image 
+          className={styles.bg} 
+          src={Background} 
+          alt="background image" 
+          width={1920} 
+          height={1275} 
+          loading="eager" 
+          onLoadingComplete={() => setIsBgLoaded(true)}
+        />
       </div>
       <div className={styles["content-layout"]}>
         <div className={styles.content}>
